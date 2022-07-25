2741b0d9968cb2440f67dbc4a566a51a2a716f06
the-scooby-gang <108406868+the-scooby-gang@users.noreply.github.com>
2022-07-25 10:18:36 +0100

    Add files via upload

---
+++ W3D5_Hwrk/W3D5_Hwrk/models/list.py
@@ -0,0 +1,10 @@
+from models.book import *
+
+book1 = Book('Fifty Shades of Grey', 'E.L.James', 'Erotica')
+book2 = Book('Braveheart', 'William Wallace', 'History')
+book3 = Book('Yer maws a binman', 'Yer das a dinner lady', 'comedy')
+books = [book1, book2, book3]
+
+
+# def add_book(book):
+#     books.append(book)
