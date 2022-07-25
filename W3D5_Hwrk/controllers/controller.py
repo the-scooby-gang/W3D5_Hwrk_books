bd5df127d860eefdddf977c1731b29d5b055b06b
the-scooby-gang <108406868+the-scooby-gang@users.noreply.github.com>
2022-07-25 08:48:34 +0100

    Add files via upload

---
+++ W3D5_Hwrk/controllers/controller.py
@@ -0,0 +1,8 @@
+from flask import render_template
+from app import app
+from models.list import books
+from models.book import Book
+
+@app.route('/books')
+def index():
+    return render_template('tasks/index.html', title = 'Todays Homework')
---
+++ W3D5_Hwrk/models/list.py
@@ -0,0 +1,9 @@
+from models.book import *
+
+book1 = Book('Fifty Shades of Grey', 'Aimee Bunyan', 'Erotica')
+book2 = Book('Braveheart', 'William Wallace', 'History')
+book3 = Book('Your mums a binman', 'your dads a dinner lady', 'comedy')
+books = [book1, book2, book3]
+
+# def add_book(book):
+#     books.append(book)
