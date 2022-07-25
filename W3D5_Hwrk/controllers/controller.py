09d654e89643474481a0e44920994dd7feb988b8
the-scooby-gang <108406868+the-scooby-gang@users.noreply.github.com>
2022-07-25 10:19:15 +0100

    Delete W3D5_Hwrk directory

--- W3D5_Hwrk/controllers/controller.py
+++
@@ -1,8 +0,0 @@
-from flask import render_template
-from app import app
-from models.list import books
-from models.book import Book
-
-@app.route('/books')
-def index():
-    return render_template('tasks/index.html', title = 'Todays Homework')
